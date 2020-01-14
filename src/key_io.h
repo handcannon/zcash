2e4a8642359f5193b690fa1ea8ead8130b9091ff
handcannon <zhining007@126.com>
2020-01-14 18:26:31 +0800

    modify genesis block

--- src/key_io.h
+++ src/key_io.h
@@ -26,6 +26,7 @@ std::string EncodeExtPubKey(const CExtPubKey& extpubkey);
 
 std::string EncodeDestination(const CTxDestination& dest);
 CTxDestination DecodeDestination(const std::string& str);
+CTxDestination DecodeDestinationRegTest(const std::string& str);
 bool IsValidDestinationString(const std::string& str);
 bool IsValidDestinationString(const std::string& str, const CChainParams& params);
 
